6d08754b5c5beb4151eda6419c5d25a0b53d326c
inno2544 <167124529+inno2544@users.noreply.github.com>
2024-10-10 01:30:45 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'My Sticker Pack',
-  author: 'My Bot',
+  author: 'Nester',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Nester', // Your name
+  ownerNumber: '254113823597', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
